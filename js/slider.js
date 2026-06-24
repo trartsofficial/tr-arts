60cedbcbeef478f6e4b2b9fb8f11c2f12b7149c3
TR Arts Official <taufiqrahmansyah006@gmail.com>
2026-06-24 10:28:56 +0700

    Initial TR Arts Website
    TR Arts Website

---
+++ js/slider.js
@@ -0,0 +1,13 @@
+const slides = document.querySelectorAll('.slide');
+
+let index = 0;
+
+setInterval(() => {
+
+  slides[index].classList.remove('active');
+
+  index = (index + 1) % slides.length;
+
+  slides[index].classList.add('active');
+
+}, 3000);
